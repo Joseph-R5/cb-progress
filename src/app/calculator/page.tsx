375c29cd5ad4cbe47e6f0bce7480e5c1aaa555d5
Joseph Rullan <joseph.rullan@gamesys.co.uk>
2023-04-09 20:51:57 +0100

    body fat percentage setup

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -1,9 +1,18 @@
-import styles from '../styles/page.module.css';
+import styles from '../styles/calculator.module.css';
+import Form from './form';
 
 const CalculatorPage = () => {
     return (
         <main className={styles.main}>
-            <h1>Calculator goes here</h1>
+            <div className={styles.container}>
+                <Form/>
+                <div className={styles.results}>
+                    <h1>Results</h1>
+                </div>
+            </div>
+            <div className={styles.reference}>
+                <h1>Ref</h1>
+            </div>
         </main>
     )
 };
