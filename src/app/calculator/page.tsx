e626c01a2440beb8da8028b46a2203c313dbcf34
Joseph Rullan <joseph.rullan@gamesys.co.uk>
2023-04-09 15:26:05 +0100

    add calculator page base

---
+++ src/app/calculator/page.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/page.module.css';
+
+const CalculatorPage = () => {
+    return (
+        <main className={styles.main}>
+            <h1>Calculator goes here</h1>
+        </main>
+    )
+};
+
+export default CalculatorPage;
--- src/app/pages/landingPage.tsx
+++ src/app/pages/landingPage.tsx
@@ -1,13 +1,18 @@
-import Navbar from "../components/Navbar"
-import GettingStarted from "../components/GettingStarted";
+import styles from '../styles/landingPage.module.css';
 
 const LandingPage = () => {
-    return (
-        <>
-            <Navbar/>
-            <GettingStarted/>
-        </>
-    )
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <h1 className={styles.title}>Big title goes here</h1>
+        <p className={styles.description}>
+          Description of the website goes here
+        </p>
+        <button className={styles.button}>Get started</button>
+      </div>
+      <div className={styles.right}></div>
+    </div>
+  )
 }
 
 export default LandingPage;
